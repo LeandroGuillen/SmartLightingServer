b50969e02a871b67567fcc6c945abd6eb5a7dbd7
JuanGarcia <juangrdiaz@gmail.com>
2013-04-06 12:31:00 +0200

    Prueba de acceso a usuarios desde nodejs

--- usuarios.js
+++ usuarios.js
@@ -1,1 +1,30 @@
 // Clase de acceso a la base de datos de usuarios desde nodejs
+
+
+var mysql  = require('mysql');
+
+
+var client = mysql.createConnection({
+  user: 'root',
+  password: 'alumno',
+  host: '127.0.0.1',
+  port: '3306',
+});
+
+client.connect();
+client.query('USE usersDB');
+
+
+
+client.query(
+    'SELECT * FROM usuarios;',
+    function selectUsuario(err, results) {
+
+    if (err) {
+        console.log("Error: " + err.message);
+        throw err;
+    }
+    console.log(results);
+
+    client.end();
+});
