5331098bad6db4c64a4ca3914506aa1d376ff73e
JuanGarcia <juangrdiaz@gmail.com>
2013-04-09 13:19:58 +0200

    Añadido control de errores en usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -10,59 +10,120 @@ var client = mysql.createConnection({
   port: '3306',
 });
 
+
 client.connect();
+
 
 client.query('use usersDB');
 
-// FALTA AÑADIR EXCEPCIONES
+
+/**
+*	Inserta usuario facilitando todos sus datos.
+*	id -> id del  usuario
+*	nombre -> nombre completo del usuario
+*	password -> hash md5 del password
+*/
+
+
 function insertUser(id,nombre,password){
 
+	client.query('INSERT INTO usuarios SET id = ?, nombre = ?, password = MD5(?)',[id,nombre,password],function (err){
 
-client.query('INSERT INTO usuarios SET id = ?, nombre = ?, password = MD5(?)',[id,nombre,password]);
+		if(err){
+	
+			connection.end();
+			throw err;
+	
+		}
 
+	});
 
+	   	
 };
 
-// FALTA AÑADIR EXCEPCIONES
+
+/**
+*	Elimina un usuario facilitando la id
+*	id -> usuario a elminar
+*/
+
 function deleteUser(id){
 
-	client.query('DELETE FROM usuarios WHERE id=?',[id]);
+	client.query('DELETE FROM usuarios WHERE id=?',[id],function (err){
+
+		if(err){
+			connection.end();
+			throw err;
+		}
+
+	});
 
 };
 
+
+/**
+*	Obtiene el nombre del usuario a partir de la id
+*	id -> id del usuario
+*/
 
 function getNombre(id){
 
-	client.query('SELECT * FROM usuarios WHERE id=?',[id], function(err, ids){
+	client.query('SELECT nombre FROM usuarios WHERE id=?',[id], function(err, nombre){
 
-		console.log(ids);});
+		if(err){
+	
+			connection.end();
+			throw err;
+	
+		}
+		
+		return nombre;
+	});
 
 };
+
+
+/**
+*	Obtiene la contraseña del usuario a partir de la id
+*	id -> id del usuario
+*/
 
 function getPassword(id){
 
 	client.query('SELECT password FROM usuarios WHERE id=?',[id], function(err, password){
+	
+		if(err){
+	
+			connection.end();
+			throw err;
+	
+		}
 
 		return password;
 	});
 
 };
 
+
 /**
-*	Funcion para comprobar la contraseña del usuario id.
+*	Comprueba la contraseña del usuario id.
 *	id -> usuario
 *	key -> hash(contraseña de id + date);
 *	Return true si es un usuario valido o false en caso contrario
 */
+
 function isAuth(id, key, date){
 
 	var md5sum  = crypto.createHash('md');
 	md5sum.update(getPassword(id)+date);
 	var digest = md5sum.digest('hex');	
 
-	return key.localeCoimpare(digest) == 0;
+	return key.localeCompare(digest) == 0;
 	
 }
+
+
+/***** Funciones publicas *****/
 
 exports.getUsuario = getUsuario;
 exports.isAuth = isAuth;
