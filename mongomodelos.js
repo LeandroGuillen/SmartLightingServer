e59bc5c76c9f0b537c7c383d223e4f777baf7abb
Leandro Guillen <leandroguillen@gmail.com>
2013-04-16 17:56:20 +0200

    Añadido modelo y coleccion para almacenar el tiempo en la base de datos MongoDB

--- mongomodelos.js
+++ mongomodelos.js
@@ -8,9 +8,21 @@ var farolaSchema = new mongoose.Schema({
 	lat: Number,
 	lon: Number
 });
+var tiempoSchema = new mongoose.Schema({
+	temperatura: Number,	// ºC
+	vientoMedio: Number,	// km/h
+	vientoRacha: Number,	// km/h
+	precipitaciones: Number, // mm
+	nubes: Number,		// %
+	humedad: Number,	// %
+	presion: Number,		// hPa
+	fecha: Date
+});
 
 // Creacion de los modelos
 var Farola = mongoose.model('Farola', farolaSchema);
+var Tiempo = mongoose.model('Tiempo', tiempoSchema);
 
 // Exportarlos para que esten disponibles en las aplicaciones
 exports.Farola = Farola;
+exports.Tiempo = Tiempo;
