8516ba512c5ba0f6bda3d473e52f452c43f3c5fc
Leandro Guillen <leandroguillen@gmail.com>
2013-04-16 17:57:12 +0200

    Provee la funcion getUltimoTiempo(). Informa sobre las condiciones meteorologicas leidas de una pagina web

--- recursos.js
+++ recursos.js
@@ -6,10 +6,16 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 var modelos = require("./mongomodelos");
 var Farola = modelos.Farola;
+var Tiempo = modelos.Tiempo;
 
 // Devuelve un JSON con todas las farolas
 function listarRecursos(callback){
-	Farola.find({encendida: false}, null, callback);
+	Farola.find({}, null, callback);
+}
+
+function getUltimoTiempo(callback) {
+	Tiempo.findOne({}, {}, { sort: { 'fecha' : -1 } }, callback);
 }
 
 exports.listarRecursos = listarRecursos;
+exports.getUltimoTiempo = getUltimoTiempo;
