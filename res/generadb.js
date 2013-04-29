73016a423679f7d8186ab471fa7f096f496a529e
LeandroGuillen <leandroguillen@gmail.com>
2013-04-29 19:43:32 +0200

    Generacion de base de datos MongoDB arreglada. Los datos ahora son coherentes. Tambien de una pasada se reinicializa la BD

--- res/generadb.js
+++ res/generadb.js
@@ -8,8 +8,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
 
-	 	crearFarolas(); // Para insertar documentos
-	// 	borrarTodo(); // Elimina bbdd
+	borrarTodo(); // Elimina bbdd
 
 });
 
@@ -19,33 +18,37 @@ function crearFarolas(){
 		nombre: 'farola1',
 		encendida: false,
 		dim: 0,
-		lat: 38023417,
-		lon: -1174624
+		lat: 38024105,
+		lon: -1173877
 	});
 	var farola2 = new Farola({
 		nombre: 'farola2',
 		encendida: false,
 		dim: 0,
-		lat: 38023419,
-		lon: -1174622
+		lat: 3802438,
+		lon: -1174083
 	});
 	var farola3 = new Farola({
 		nombre: 'farola3',
 		encendida: true,
 		dim: 100,
-		lat: 38023419,
-		lon: -1174620
+		lat: 38024635,
+		lon: -1174266
 	});
 	
 	// Guardar documentos
 	farola1.save();
 	farola2.save();
 	farola3.save();
+	
+	console.log("Farolas re-creadas");
 }
 
 function borrarTodo(){
 	// Vaciar colecciones
 	Farola.remove({}, function(err){
 		console.log('Coleccion borrada de la base de datos.');
+		
+		crearFarolas(); // Para insertar documentos
 	});
 }
